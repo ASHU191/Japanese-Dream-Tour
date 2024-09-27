838cb695c165fdf7d1d6f56362e0206d4df2f00a
MUHAMMAD ARSALAN AFTAB <106240658+ASHU191@users.noreply.github.com>
2024-09-27 23:18:50 +0500

    Update tourRoutes.js

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -12,7 +12,7 @@ const storage = multer.diskStorage({
     filename: (req, file, cb) => {
       cb(null, `${Date.now()}-${file.originalname}`);
     }
-  });
+  });    
   
   const upload = multer({ storage });
   
