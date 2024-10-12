73012fc1ba1b1adc1d28bc040f679a0b89efe641
MUHAMMAD ARSALAN AFTAB <106240658+ASHU191@users.noreply.github.com>
2024-10-13 01:59:02 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const config = require("./config/db.js");
+const config = require("./config/db.js");   
 require("dotenv").config();   
 
 // Import routes
