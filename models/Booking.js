0802da18fa762a56e666fd5da44292fc802df827
MUHAMMAD ARSALAN AFTAB <106240658+ASHU191@users.noreply.github.com>
2024-10-12 12:22:05 +0500

    Update Booking.js

--- models/Booking.js
+++ models/Booking.js
@@ -4,7 +4,7 @@ const BookingSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   destination: { type: mongoose.Schema.Types.ObjectId, ref: 'Destination', required: true },
   date: { type: Date, required: true },
-  status: { type: String, enum: ['Pending', 'Confirmed', 'Cancelled'], required: true }
+  status: { type: String, enum: ['Pending', 'Confirmed', 'Cancelled'], required: true }                                                              
 });
 
 module.exports = mongoose.model('Booking', BookingSchema);
