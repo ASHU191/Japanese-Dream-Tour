703992f80b26ba0e90e08e714945b2ec0abdbd74
MUHAMMAD ARSALAN AFTAB <106240658+ASHU191@users.noreply.github.com>
2024-10-13 23:56:49 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"); 
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const config = require("./config/db.js");   
