164621e5fce0d788675c04cc3aa744cbd14d1163
MUHAMMAD ARSALAN AFTAB <106240658+ASHU191@users.noreply.github.com>
2024-10-16 15:39:53 +0500

    Update Content.js

--- models/Content.js
+++ models/Content.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const ContactSchema = new mongoose.Schema({
   title: { type: String, required: true },    
   gmail: { type: String, required: true },
-  fullName: { type: String, required: true },                                                               
+  fullName: { type: String, required: true }, 
+  
   description: { type: String, required: true }
 });
 
