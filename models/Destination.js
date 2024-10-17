6592f24ae77d31cf755c7253464a122a2bf29bf9
MUHAMMAD ARSALAN AFTAB <106240658+ASHU191@users.noreply.github.com>
2024-10-17 12:37:17 +0500

    Update Destination.js

--- models/Destination.js
+++ models/Destination.js
@@ -4,6 +4,7 @@ const DestinationSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   destinationImage: { type: String }
+  
 });
 
 module.exports = mongoose.model('Destination', DestinationSchema);
